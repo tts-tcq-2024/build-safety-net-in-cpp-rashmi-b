be0ded1bdc4c4eebd06e022939425cdde092a655
Ballamajalu Rashmi (SX/ETS1-MM) <arl9kor@KOR-C-013HJ.kor.apac.bosch.com>
2024-07-05 13:18:00 +0530

    Updated Map creation outside of function

---
+++ Soundex.Tests.Specs.md
@@ -0,0 +1,7 @@
+<!-- Test Specefications doucment for Soundex Implementation -->
+# Test cases
+* If empty string is given, output should return empty string i.e. ""
+* If Single character input is given, output should return same as fisrt charater appended by zeroes. E.g. "A" is "A000"
+* If Repeat of same character is given, output returns first character appended by zeroes. E.g. "Aaa" is "A000"
+* If word is less than 4 charaters, output returns code appended by zeroes until it is 4 character code. E.g. "Set" is "S300"
+* If a multiple charater word is given, output returns the code upto 4 characters. e.g. "Robert" is "R163"
--- Soundex.cpp
+++ Soundex.cpp
@@ -2,16 +2,18 @@
 #include <cctype>
 #include <map>
 
+//Declare and define Map here globally
+std::map<char,char> charMap = { {'B','1'}, {'F','1'}, {'P','1'}, {'V','1'},
+                        {'C','2'}, {'G','2'}, {'J','2'}, {'K','2'}, {'Q','2'}, {'S','2'}, {'X','2'}, {'Z','2'},
+                        {'D','3'}, {'T','3'},
+                        {'L','4'},
+                        {'M','5'}, {'N','5'},
+                        {'R','6'},
+                        {'A','0'}, {'E','0'}, {'I','0'}, {'O','0'}, {'U','0'}, {'H','0'}, {'W','0'}, {'Y','0'}
+                        };
+
 char getSoundexCode(char c) {
     c = toupper(c);
-    std::map<char,char> charMap = { {'B','1'}, {'F','1'}, {'P','1'}, {'V','1'},
-                           {'C','2'}, {'G','2'}, {'J','2'}, {'K','2'}, {'Q','2'}, {'S','2'}, {'X','2'}, {'Z','2'},
-                           {'D','3'}, {'T','3'},
-                           {'L','4'},
-                           {'M','5'}, {'N','5'},
-                           {'R','6'},
-                           {'A','0'}, {'E','0'}, {'I','0'}, {'O','0'}, {'U','0'}, {'H','0'}, {'W','0'}, {'Y','0'}
-                           };
     // return Map consonants to digits
     for (auto charValue : charMap) // Look at each key-value pair
     {
